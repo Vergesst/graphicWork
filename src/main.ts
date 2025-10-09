23d77eb31cf3ec6edf7cff40b30f04ce4e699a58
Vergesst <3224171967@qq.com>
2025-10-09 23:59:19 +0800

    feat: simple logic added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,61 @@
-import { FabricText, StaticCanvas } from 'fabric'
+import { FabricText, Canvas, Rect, InteractiveFabricObject } from 'fabric'
+const canvasE1 = document.getElementById('canvas') as HTMLCanvasElement
+const canvas = new Canvas(canvasE1)
 
-FabricText.ownDefaults.fontFamily = 'Lobster'
+InteractiveFabricObject.ownDefaults = {
+    ...InteractiveFabricObject.ownDefaults,
+    cornerStrokeColor: 'blue',
+    cornerColor: 'lightblue',
+    cornerStyle: 'circle',
+    padding: 10,
+    transparentCorners: false,
+    cornerDashArray: [2, 2],
+    borderColor: 'orange',
+    borderDashArray: [3, 1, 3],
+    borderScaleFactor: 2,
+}
 
-let otherFonts = new FabricText('Another text', { fontFamily: 'Arial' })
+const text = new FabricText('Fabric.JS')
+const rect = new Rect({ width: 100, height: 100, fill: 'green' })
 
-const canvas = new StaticCanvas('canvas')
+canvas.add(text, rect)
+canvas.centerObject(text)
+canvas.setActiveObject(text)
 
-canvas.add(otherFonts)
-canvas.renderAll()
+// get html elements
+const remove_btn = document.getElementById('remove-btn') as HTMLButtonElement
+const color_btn = document.getElementById('color-btn') as HTMLButtonElement
+const color_picker = document.getElementById('color-picker') as HTMLInputElement
+
+remove_btn.addEventListener('click', () => {
+  // process all selected objects
+  const active_objects = canvas.getActiveObjects()
+
+  if (!active_objects || active_objects.length === 0) {
+    alert('nothing to remove')
+    return
+  }
+
+  active_objects.forEach(it => {
+    canvas.remove(it)
+  })
+  canvas.discardActiveObject()
+
+  canvas.renderAll()
+})
+
+color_btn.addEventListener('click', () => {
+  color_picker.click()
+})
+
+color_picker.addEventListener('input', (e) => {
+  const new_color = (e.target as HTMLInputElement).value
+
+  const active_objects = canvas.getActiveObjects()
+  if (active_objects.length > 0) {
+    active_objects.forEach(it => {
+      it.set('fill', new_color)
+    })
+    canvas.renderAll()
+  }
+})
